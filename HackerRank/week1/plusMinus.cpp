ede017c61b2d6788280bfca2b61bcb5c23efeda0
AJITHKUMAR S <cloudwithajith@gmail.com>
2025-08-05 23:53:17 +0530

    Update plusMinus.cpp

--- HackerRank/week1/plusMinus.cpp
+++ HackerRank/week1/plusMinus.cpp
@@ -2,8 +2,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ * vector<int> is used to interact with the arr 
+ * arr.size ---> to give total element in the arr
+ * 
+ */
+ 
+ 
+// answer-----------------------
+void plusMinus(vector<int> arr) {
+    int n  = arr.size();
+    
+    int zero=0, pos=0, neg=0;
+    
+    for(int number : arr){
+        if(number>0)pos++;
+        else if (number<0)neg++;
+        else zero++;
+        
+    }
+    
+    
+    cout<<(double)pos/n<<endl;
+    cout<<(double)neg/n<<endl;
+    cout<<(double)zero/n<<endl;
 
+}
