42f6cc684a507acaeebfba6f380e41112d99350f
AJITHKUMAR S <cloudwithajith@gmail.com>
2025-08-07 20:36:05 +0530

    Update Timecover.cpp

--- HackerRank/week1/Timecover.cpp
+++ HackerRank/week1/Timecover.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 
 string timeConversion(string s) { 
-    string p = s.substr(8, 2); // AM or PM
-    int hour = stoi(s.substr(0, 2)); // First 2 characters
+    string p = s.substr(8, 2); 
+    int hour = stoi(s.substr(0, 2)); 
 
     if (p == "AM") {
         if (hour == 12)
-            hour = 0; // 12AM -> 00
+            hour = 0; 
     } else { // PM
         if (hour != 12)
-            hour += 12; // Add 12 for PM hours (except 12PM)
+            hour += 12; 
     }
 
     stringstream results;
-    results << setw(2) << setfill('0') << hour << s.substr(2, 6); // :MM:SS
+    results << setw(2) << setfill('0') << hour << s.substr(2, 6); 
 
     return results.str();
+    
 
 
 }
