eb6b4871a7c46522122c25d09cac370be84c94c9
AJITHKUMAR S <cloudwithajith@gmail.com>
2025-08-08 21:39:22 +0530

    Update SpaceArray.cpp

--- HackerRank/week1/SpaceArray.cpp
+++ HackerRank/week1/SpaceArray.cpp
@@ -1,1 +1,20 @@
-a
+//Space Array
+
+int* r = malloc(queries_count * sizeof(int));
+
+    for (int i = 0; i < queries_count; i++) {
+        int count = 0;
+
+
+        for (int j = 0; j < strings_count; j++) {
+            if (strcmp(queries[i], strings[j]) == 0) {
+                count++;
+            }
+        }
+
+        r[i] = count; 
+    }
+
+    *result_count = queries_count; 
+    return r;
+}
