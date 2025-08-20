de7c0dc2d6c9a44e68ac5cd22bfd3b75b537e0ab
AJITHKUMAR S <cloudwithajith@gmail.com>
2025-08-20 23:39:20 +0530

    Create CaesarCipher.cpp

---
+++ HackerRank/week2/CaesarCipher.cpp
@@ -0,0 +1,19 @@
+char* caesarCipher(char* s, int k) {
+    int len = strlen(s);
+    char* result = malloc((len + 1) * sizeof(char)); 
+    k = k % 26;  
+    for (int i = 0; i < len; i++) {
+        char c = s[i];
+
+        if (c >= 'a' && c <= 'z') { 
+            result[i] = ((c - 'a' + k) % 26) + 'a';
+        } else if (c >= 'A' && c <= 'Z') {  
+            result[i] = ((c - 'A' + k) % 26) + 'A';
+        } else {
+            result[i] = c;  
+        }
+    }
+
+    result[len] = '\0'; 
+    return result;
+}
